91512b611949bc60593f3bbc01716e182ef46b82
eason <lolmuta@gmail.com>
2024-03-08 14:35:39 +0800

    加入專案檔案。

---
+++ BinaryHeapReCod/BinaryHeap.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Text;
+
+namespace BinaryHeapReCod
+{
+    internal class BinaryHeap
+    {
+        public int Size { get; private set; }
+        public int Count { get; private set; }
+        public int[] Arr;
+        public BinaryHeap(int Size)
+        {
+            this.Size = Size;
+            Arr = new int[Size];
+        }
+        private int GetParentIndex(int index)
+        {
+            return (index - 1) / 2;
+        }
+        private int GetChildLeft(int index)
+        {
+            return 2 * index + 1;
+        }
+        private int GetChildRight(int index)
+        {
+            return 2 * index + 2;
+        }
+        internal void Push(int v)
+        {
+            Count++;
+            Arr[Count - 1] = v;
+            BinaryHeapOrderUp(Count - 1);
+        }
+
+        private void BinaryHeapOrderUp(int index)
+        {
+            while (index > 0)
+            {
+                var parentIndex = GetParentIndex(index);
+                if (Arr[parentIndex] > Arr[index])
+                {
+                    Swap(parentIndex, index);
+                    index = parentIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Swap(int parentIndex, int index)
+        {
+            int temp = Arr[parentIndex];
+            Arr[parentIndex] = Arr[index];
+            Arr[index] = temp;
+        }
+
+        internal int Pop()
+        {
+            int result = Arr[0];
+            Arr[0] = Arr[Count - 1];
+            Count--;
+            BinaryHeapOrderDown();
+            return result;
+        }
+        private void BinaryHeapOrderDown()
+        {
+            int index = 0;
+
+            while (true)
+            {
+                var ChildLeftIndex = GetChildLeft(index);
+                var ChildRightIndex = GetChildRight(index);
+                var ExistChildLeft = ChildLeftIndex < Count;
+                var ExistChildRight = ChildRightIndex < Count;
+
+                if (!ExistChildLeft && !ExistChildRight)
+                {
+                    // No child then exists
+                    break;
+                }
+
+                int smallerChildIndex = ChildLeftIndex;
+                if (ExistChildRight && Arr[ChildRightIndex] < Arr[ChildLeftIndex])
+                {
+                    smallerChildIndex = ChildRightIndex;
+                }
+
+                if (Arr[index] > Arr[smallerChildIndex])
+                {
+                    Swap(index, smallerChildIndex);
+                    index = smallerChildIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        public void Print()
+        {
+            //2^0 + 2^1 + 2^2 ...
+            //2 ^ n 
+            //1(2^n -1)
+            var sb = new StringBuilder();
+            int lv = 1;
+            for (int i = 0; i < Count; i++)
+            {
+                var lvMaxCount = (int)Math.Pow(2, lv) - 1;
+                if (i+1 > lvMaxCount)
+                {
+                    lv++;
+                    sb.AppendLine("");
+                }
+                sb.Append(Arr[i] + ",");
+            }
+            sb.AppendLine();
+            sb.AppendLine(new string('*', 10));
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ BinaryHeapReCod/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryHeapReCod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = new List<int>()
+            {
+                6214, 8543, 9266 ,1150 ,
+                7498, 7209, 9398, 1529,
+                1032, 7384, 6784, 34,
+                1449, 7598, 8795, 756,
+                7803, 4112, 298, 4967,
+                1261, 1724, 4272, 1100,
+                9373
+            };
+            var result = cookies(3581, A);
+            //BinaryHeap binaryHeap = new BinaryHeap(100);
+            //binaryHeap.Push(1123);                  binaryHeap.Print();
+            //binaryHeap.Push(4);                     binaryHeap.Print();
+            //binaryHeap.Push(1);                     binaryHeap.Print();
+            //binaryHeap.Push(6);                     binaryHeap.Print();
+            //binaryHeap.Push(7);                     binaryHeap.Print();
+            //binaryHeap.Push(9);                     binaryHeap.Print();
+            //binaryHeap.Push(12);                    binaryHeap.Print();
+            //binaryHeap.Push(16);                    binaryHeap.Print();
+            //Console.WriteLine(binaryHeap.Pop());    binaryHeap.Print();
+            //Console.WriteLine(binaryHeap.Pop());    binaryHeap.Print();
+            //Console.WriteLine(binaryHeap.Pop());    binaryHeap.Print();
+            //Console.WriteLine(binaryHeap.Pop());    binaryHeap.Print();
+            //Console.WriteLine(binaryHeap.Pop());    binaryHeap.Print();
+        }
+        public static int cookies(int k, List<int> A)
+        {
+            BinaryHeap binaryHeap = new BinaryHeap(A.Count);
+            for (int i = 0; i < A.Count; i++)
+            {
+                binaryHeap.Push(A[i]);
+            }
+            binaryHeap.Print();
+            int WhileCount = 0;
+            int min0, min1;
+            while (true)
+            {
+                min0 = binaryHeap.Pop();
+                if (min0 < k && binaryHeap.Count > 0)
+                {
+                    WhileCount++;
+                    min1 = binaryHeap.Pop();
+                    var sweetness = min0 + 2 * min1;
+                    binaryHeap.Push(sweetness);
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            if (min0 < k)
+            {
+                return -1;
+            }
+            binaryHeap.Print();
+            return WhileCount;
+        }
+    }
+}
